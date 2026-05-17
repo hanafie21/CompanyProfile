dbd1249b0833266f80969fdd9b1e1442592f1398
Hanafie Rizal Fathoni <hanafirizal7528@gmail.com>
2026-05-17 16:15:00 +0700

    menambahkan fitur perkalian

--- fungsi.js
+++ fungsi.js
@@ -3,3 +3,7 @@ const add = (a , b) => a + b;
 console.log(add(5, 10));
 
 const subtract = (a , b) => a - b;
+console.log(subtract(10, 5));
+
+const multiply = (a , b) => a * b;  
+console.log(multiply(5, 10));
